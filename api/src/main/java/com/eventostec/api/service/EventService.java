642dc426dc7527cad4f312c5993b4a5950bfa3b1
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-06-19 01:18:28 -0300

    Criado um novo DTO para event e função de paginação

--- api/src/main/java/com/eventostec/api/service/EventService.java
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -3,9 +3,13 @@ package com.eventostec.api.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +17,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -42,9 +47,14 @@ public class EventService {
         event.setRemote(data.remote());
 
         eventRepository.save(event);
-
         return event;
+    }
 
+    public List<EventResponseDTO> getEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.eventRepository.findAll(pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(),event.getDate(),
+                "", "", event.isRemote(), event.getEventUrl(), event.getImgUrl())).stream().toList();
     }
 
     private String uploadImg(MultipartFile multipartFile) {
