e5cc8aafaa748ba7522166a7e35083f38d2cccdb
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-06-22 23:18:55 -0300

    Começando um novo projeto, com a utilização do MongoDB

---
+++ whorkshop/src/test/java/com/julio/whorkshop/WhorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.whorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
