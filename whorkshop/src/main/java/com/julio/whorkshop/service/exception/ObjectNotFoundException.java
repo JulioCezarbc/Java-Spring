93e104e30791c8a0b82d044d02fa17e5d8e08c46
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-30 01:15:41 -0300

    Tratando exceptions

---
+++ whorkshop/src/main/java/com/julio/whorkshop/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.julio.whorkshop.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
