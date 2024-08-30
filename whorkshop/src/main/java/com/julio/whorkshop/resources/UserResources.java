730f746810641ad9213df8d52d55983c4c00a5ff
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-30 01:15:20 -0300

    Obtendo o usuario por id

--- whorkshop/src/main/java/com/julio/whorkshop/resources/UserResources.java
+++ whorkshop/src/main/java/com/julio/whorkshop/resources/UserResources.java
@@ -5,6 +5,7 @@ import com.julio.whorkshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,10 @@ public class UserResources {
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 
--- whorkshop/src/main/java/com/julio/whorkshop/service/UserService.java
+++ whorkshop/src/main/java/com/julio/whorkshop/service/UserService.java
@@ -3,10 +3,12 @@ package com.julio.whorkshop.service;
 import com.julio.whorkshop.DTO.UserDTO;
 import com.julio.whorkshop.domain.User;
 import com.julio.whorkshop.repository.UserRepository;
+import com.julio.whorkshop.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,6 +19,10 @@ public class UserService {
     public List<UserDTO> findAll(){
         List<User> list = repository.findAll();
         return list.stream().map(user -> new UserDTO(user.getId(), user.getName(), user.getEmail())).toList();
+    }
+    public UserDTO findById(String id){
+        User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
+        return new UserDTO(user.getId(),user.getName(),user.getEmail());
     }
 
 }
