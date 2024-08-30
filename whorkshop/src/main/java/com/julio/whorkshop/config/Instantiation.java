109f2a4b8367ad9487d5066033c7eca2c5180857
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-30 12:35:28 -0300

    Criando EntityPost, com o user aninhado

--- whorkshop/src/main/java/com/julio/whorkshop/config/Instantiation.java
+++ whorkshop/src/main/java/com/julio/whorkshop/config/Instantiation.java
@@ -1,26 +1,40 @@
 package com.julio.whorkshop.config;
 
+import com.julio.whorkshop.domain.Post;
 import com.julio.whorkshop.domain.User;
+import com.julio.whorkshop.repository.PostRepository;
 import com.julio.whorkshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private PostRepository postRepository;
     @Override
     public void run(String... args) throws Exception {
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         repository.deleteAll();
+        postRepository.deleteAll();
+
         User jay = new User(null, "jay", "jay@gmail.com");
         User juy = new User(null, "juy", "juy@gmail.com");
         User bob = new User(null, "bob","bob@gmail.com");
 
+        Post p1 = new Post(null, sdf.parse("30/08/2024"),"Partiu viagem!","Indo para o interior", jay);
+        Post p2 = new Post(null, sdf.parse("03/09/2024"),"Bom dia!","Acordei pampa hoje", jay);
+
+
         repository.saveAll(Arrays.asList(jay,juy,bob));
+        postRepository.saveAll(Arrays.asList(p1,p2));
     }
 }
---
+++ whorkshop/src/main/java/com/julio/whorkshop/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.julio.whorkshop.repository;
+
+import com.julio.whorkshop.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+}
